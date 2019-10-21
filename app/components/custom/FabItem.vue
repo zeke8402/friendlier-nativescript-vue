235834016a87d203eb6a6fa4b06840c1a096de4d
Zeke <jkezekiel@gmail.com>
2019-10-20 19:50:13 -0500

    new nav implemented but it's missing clickable functionality. click drops to background cell instead of button

---
+++ app/components/custom/FabItem.vue
@@ -0,0 +1,29 @@
+<template>
+	<StackLayout class="fab-item" :backgroundColor="color" @onButtonTap="testWhatever">
+		<Label :text="title" horizontalAlignment="center" />
+	</StackLayout>
+</template>
+
+<script>
+	export default {
+		methods: {
+			testWhatever() {
+				console.log('testing')
+			}
+		},
+	props: { title: String, color: String }
+}
+
+</script>
+
+<style scoped>
+	.fab-item {
+		width: 40;
+		height: 40;
+		border-radius: 20;
+		color: #fff;
+		font-weight: bold;
+		vertical-align: center;
+		z-index: 9999;
+	}
+</style>
